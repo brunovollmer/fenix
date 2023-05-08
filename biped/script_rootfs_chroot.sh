c2f37a573913dc96e1c5d31cab38856cbe149a13
Nicooow <nicolas.boujot@icloud.com>
2023-05-08 17:02:02 +0200

    fixs + cmakes .deb + prebuilt package + postbuild script

--- biped/script_rootfs_chroot.sh
+++ biped/script_rootfs_chroot.sh
@@ -10,18 +10,39 @@ esac
 
 echob () {
     echo "==BIPED-CHROOT==" $@
+    # echo in stderr too for debug
+    echo "==BIPED-CHROOT==" $@ >&2
+}
+
+# create a function is_directory_exist to check if directory exist
+is_directory_exist() {
+    if [ -d "$1" ]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 install_bluez() {
     # upgrade bluetooth
-    cd /tmp/biped/reqs/bluez-5.65
+    PACKAGE="bluez-5.65"
+    echob "Installing $PACKAGE"
 
-    echob "Installing bluez"
-    ./bootstrap
-    ./configure --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var
-    make -j8
+    # check if not is_directory_exist
+    if ! is_directory_exist "/tmp/biped/build_reqs/$PACKAGE/"; then
+        cd /tmp/biped/reqs/$PACKAGE
+        echob "$PACKAGE is not already built, building now"
+        ./bootstrap
+        ./configure --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var
+        make -j8
+        mv /tmp/biped/reqs/$PACKAGE /tmp/biped/build_reqs/$PACKAGE
+    else
+        echob "$PACKAGE is already built, skipping build"
+    fi
+
+    cd /tmp/biped/build_reqs/$PACKAGE
     make install
-    echob "Installing bluez done"
+    echob "Installing $PACKAGE done"
 
     # link new bluetooth config
     echob "Linking new bluetooth config"
@@ -34,56 +55,114 @@ install_bluez() {
 
 install_python() {
     # install python3.8.15
-    cd /tmp/biped/reqs/Python-3.8.15
+    PACKAGE="Python-3.8.15"
+    echob "Installing $PACKAGE"
 
-    echob "Installing python3.8.15"
-    ./configure --enable-optimizations
-    make -j8
+    # check if not is_directory_exist
+    if ! is_directory_exist "/tmp/biped/build_reqs/$PACKAGE/"; then
+        cd /tmp/biped/reqs/$PACKAGE
+        echob "$PACKAGE is not already built, building now"
+        ./configure --enable-optimizations
+        make -j8
+        mv /tmp/biped/reqs/$PACKAGE /tmp/biped/build_reqs/$PACKAGE
+    else
+        echob "$PACKAGE is already built, skipping build"
+    fi
+
+    cd /tmp/biped/build_reqs/$PACKAGE
     make altinstall
-    echob "Installing python3.8.15 done"
+    echob "Installing $PACKAGE done"
 }
 
 install_cmake() {
     # install cmake3.18.4
-    cd /tmp/biped/reqs/cmake-3.18.4/
+    PACKAGE="cmake-3.18.4"
+    echob "Installing $PACKAGE"
 
-    echob "Installing cmake3.18.4"
-    rm /tmp/biped/reqs/cmake-3.18.4/CMakeCache.txt
-    ./configure
-    make -j6
-    make install
-    echob "Installing cmake3.18.4 done"
+    # # check if not is_directory_exist
+    # if ! is_directory_exist "/tmp/biped/build_reqs/$PACKAGE/"; then
+    #     echob "$PACKAGE is not already built, building now"
+    #     cd /tmp/biped/reqs/$PACKAGE
+    #     rm /tmp/biped/reqs/cmake-3.18.4/CMakeCache.txt
+    #     ./configure
+    #     make -j8
+    # else
+    #     echob "$PACKAGE is already built, skipping build, just configuring"
+    #     rm -rf /tmp/biped/reqs/$PACKAGE
+    #     mv /tmp/biped/build_reqs/$PACKAGE /tmp/biped/reqs/
+    #     cd /tmp/biped/reqs/$PACKAGE
+    #     ./configure
+    # fi
+
+    # cd /tmp/biped/reqs/$PACKAGE
+    # ./bootstrap
+    # make -j8
+    # make install
+    # rm -rf /tmp/biped/build_reqs/$PACKAGE
+    # cp /tmp/biped/reqs/$PACKAGE /tmp/biped/build_reqs/
+
+    # build method is very very slow, so we just install from deb file
+    cd /tmp/biped/reqs/cmake
+
+    dpkg -i libjsoncpp24_1.9.4-4_arm64.deb
+    dpkg -i librhash0_1.3.8-1_arm64.deb
+    dpkg -i cmake-data_3.18.4-2+deb11u1_all.deb
+    dpkg -i cmake_3.18.4-2+deb11u1_arm64.deb
+
+    echob "Installing $PACKAGE done"
 }
 
 install_librealsense() {
     # install librealsense2.53.1
-    cd /tmp/biped/reqs/librealsense-2.53.1
+    # installing in /home/khadas/ folder else it will not work
+    # (deleting after)
+    PACKAGE="librealsense-2.53.1"
+    echob "Installing $PACKAGE"
 
-    echob "Installing librealsense2.53.1"
-    mkdir build && cd build
-    cmake ../ -DBUILD_PYTHON_BINDINGS:bool=true -DFORCE_RSUSB_BACKEND:bool=true -DBUILD_WITH_CUDA:bool=false -DBUILD_GRAPHICAL_EXAMPLES:bool=false -DCMAKE_BUILD_TYPE=release
-    make -j8
+    mkdir -p /home/khadas/$PACKAGE
+
+    python3.8 -m pip install --user virtualenv
+    cd /tmp/biped/
+    python3.8 -m venv env
+    source env/bin/activate
+    # Now check that
+    python --version
+    # shows 3.8.15
+
+    # check if not is_directory_exist
+    if ! is_directory_exist "/tmp/biped/build_reqs/$PACKAGE/"; then
+        mv /tmp/biped/reqs/$PACKAGE /home/khadas
+        cd /home/khadas/$PACKAGE
+        echob "$PACKAGE is not already built, building now"
+        mkdir build && cd build
+        /usr/bin/cmake ../ -DBUILD_PYTHON_BINDINGS:bool=true -DFORCE_RSUSB_BACKEND:bool=true -DBUILD_WITH_CUDA:bool=false -DBUILD_GRAPHICAL_EXAMPLES:bool=false -DCMAKE_BUILD_TYPE=release
+        make -j8
+    else
+        echob "$PACKAGE is already built, skipping build"
+        mv /tmp/biped/build_reqs/$PACKAGE /home/khadas
+    fi
 
     # set udev rules before install
-    cd /tmp/biped/reqs/librealsense-2.53.1
+    cd /home/khadas/$PACKAGE
     yes | bash -e ./scripts/setup_udev_rules.sh
-
     cd ./build/
     make install
-    echob "Installing librealsense2.53.1 done"
+    cp -r /home/khadas/$PACKAGE /tmp/biped/build_reqs/
+    cd /home
+    rm -rf /home/khadas/$PACKAGE
+    echob "Installing $PACKAGE done"
 }
 
 apt full-upgrade -y
 sync
 
 ln -s /tmp/biped/reqs /home/khadas/reqs
+mkdir -p /tmp/biped/build_reqs/
 
 install_bluez
 install_python
 install_cmake
 install_librealsense
 
-# delete temp folder for reduce final image size
-rm -rf /tmp/biped
 # Self-deleting
 rm $0
